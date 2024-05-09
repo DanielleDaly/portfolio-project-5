44130258f420d7e07765bc673b35cfcc5db7aea6
DanielleDaly <daniellerdaly@gmail.com>
2024-05-09 11:45:33 +0000

    Remove secret key and set debug

--- books_bazaar/settings.py
+++ books_bazaar/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&pfe&&v1*%g2x(2nm(%-o8phj0wv%c^2mmamzdlofaf&u%3azh'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['books-bazaar.herokuapp.com', 'books-bazaar-08785163d42b.herokuapp.com', 'localhost']
 
