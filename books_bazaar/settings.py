4433e644c8880deff16fa1d3dff24afa24abad2f
DanielleDaly <daniellerdaly@gmail.com>
2024-05-10 08:14:51 +0000

    Update python version in runtine.txt for email sending

--- books_bazaar/settings.py
+++ books_bazaar/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['books-bazaar.herokuapp.com', 'books-bazaar-08785163d42b.herokuapp.com', 'localhost']
 
