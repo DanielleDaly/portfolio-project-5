5f0c7f8df944d737c45cb4cf8df65d6253d9cbf6
DanielleDaly <daniellerdaly@gmail.com>
2024-05-10 06:41:45 +0000

    Regenerate sitemap after adding reviews content

--- books_bazaar/settings.py
+++ books_bazaar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '57r0wvrim*@8inn!7*e5rwc(s+rnlh&ymjtprmtwgy2o=3uko3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
